0d7d368a7e8d2e518b37bf00cbde5a0126ab50af
Víctor Torres García <victor@victor-VirtualBox.(none)>
2014-03-10 00:39:35 +0100

    comensales

--- recuperacion/ruby/rubystatements/comensales.rb
+++ recuperacion/ruby/rubystatements/comensales.rb
@@ -1,36 +1,52 @@
 #!/usr/bin/env ruby
 
-
-def accion_comensal (numeroComensal)
-	loop do
-	coger_cubierto(numeroComensal)
-	if(@@cantidad_cubiertos == 2)
-	sleep rand 8
-	puts "Comensal #{numeroComensal} comiendo"
-	sleep rand 8
-	puts "Comensal #{numeroComensal} descansando"
-end
+class CubiertoIzquierda
+	@@cubierto = "Izquierdo"
+	def initialize
+	@cubierto = @@cubierto
 	end
+	def cubiertoIzq?
+	@cubierto
+	end	
 end
 
-@@cantidad_cubiertos = 0
+class CubiertoDerecha
+	@@cubierto = "Derecho"
+	def initialize
+	@cubierto = @@cubierto
+	end
+	def cubiertoDer?
+	@cubierto
+	end	
+end
 
-def coger_cubierto (nComensal)
-	while @@cantidad_cubiertos < 2
-	@@cantidad_cubiertos +=1
-	puts "Comensal #{nComensal} ha cogido el cubierto #{@@cantidad_cubiertos}"
-	sleep rand 5
-	@@cantidad_cubiertos = 0
+
+def accion_comensal (numeroComensal, cubiertoI, cubiertoD)
+	loop do
+	sleep rand(8)
+	puts "Comensal #{numeroComensal} ha cogido el cubierto #{cubiertoI} "
+	sleep rand(8)
+	puts "Comensal #{numeroComensal} ha cogido el cubierto #{cubiertoD} "
+	sleep rand(8)
+	puts "Comensal #{numeroComensal} comiendo"
+	sleep rand(8)
+	puts "Comensal #{numeroComensal} descansando"
 	end
 end
 
 comensales = []
 numero_comensal = 0
 
+#puts "#{cubierto_izq} #{cubierto_der}\n"
+cantidad_cubiertos = 0
+cubierto_derecho_cogido = false
+
 while comensales.length<4
  		comensales << Thread.new do
  		numero_comensal +=1
-	accion_comensal(numero_comensal)
+	cub1 = CubiertoIzquierda.new
+	cub2 = CubiertoDerecha.new
+	accion_comensal(numero_comensal, cub1.cubiertoIzq?, cub2.cubiertoDer?)
 	end
 end
 
